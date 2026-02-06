b8118c3d4dcb2d4840f9a81bf7fd1a172d9e98dd
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2026-02-06 18:02:55 +0100

    create database, migrations, models, seeder

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+    ];
+
+
+    public function users(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class,'post_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Log;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +49,31 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function userPosts(){
+        return $this->hasMany(Post::class,'user_id');
+    }
+
+    public function userComments(){
+        return $this->hasMany(Comment::class,'user_id');
+    }
+
+    protected static function booted() // itt állítjuk be a hozzá kapcsolód táblák rekordjait is
+    {
+        static::deleting(function ($user) {
+            $user->userPosts()->delete(); // beállítás a hozzá kapcsolódó táblához is, h annak a rekordjai is "törlődjenek"
+            $user->userComments()->delete();
+        });
+
+        static::restoring(function ($user) {
+            Log::info("User restored: " . $user->id);
+            $user->userPosts()->withTrashed()->restore(); //withtrashed biztisít h a törölt rekordok visszaállnak
+            $user->userComments()->withTrashed()->restore(); // visszaállítjuk a törölt profilképet
+
+
+
+        });
+    }
+
+
 }
